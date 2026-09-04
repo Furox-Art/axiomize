4467bcdaecc743fd7dce6e91c023a973ae52ad6b
Furox <real.furkannn@gmail.com>
2026-09-04 21:48:17 +0300

    Add parameter export package

---
+++ src/axiomize/parameters/__init__.py
@@ -0,0 +1,5 @@
+"""Parameter-table parsing and export utilities."""
+
+from axiomize.parameters.export import export_parameter_table, parse_parameter_table
+
+__all__ = ["export_parameter_table", "parse_parameter_table"]
