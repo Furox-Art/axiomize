867575c42e7746894782f055b66ff45f4c1b1713
Furox <real.furkannn@gmail.com>
2026-09-04 23:33:02 +0300

    Add scientific visualization package

---
+++ src/axiomize/visualization/__init__.py
@@ -0,0 +1,9 @@
+"""Matplotlib-based scientific visualization helpers."""
+
+from axiomize.visualization.plots import (
+    plot_dependency_graph,
+    plot_sensitivity,
+    plot_surface_3d,
+)
+
+__all__ = ["plot_dependency_graph", "plot_sensitivity", "plot_surface_3d"]
